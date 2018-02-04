9cd033a89f3a4db3e3bd8e2c9af6ec959da661c4
amyjzhu <ajyzhu@gmail.com>
2018-02-04 14:53:46 -0800

    Add sql dump files

---
+++ monster.sql
@@ -0,0 +1,237 @@
+-- MySQL dump 10.16  Distrib 10.1.17-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: monster
+-- ------------------------------------------------------
+-- Server version       10.1.17-MariaDB-1~jessie
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `action`
+--
+
+DROP TABLE IF EXISTS `action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `action` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'the attack''s id',
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `type` varchar(50) NOT NULL DEFAULT 'action' COMMENT 'action, legendary_action, or attack',
+  `desc` varchar(200) NOT NULL DEFAULT '',
+  `attack_bonus` int(11) NOT NULL DEFAULT '0',
+  `damage_dice` varchar(20) NOT NULL DEFAULT '0d0',
+  `damage_bonus` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `action`
+--
+
+LOCK TABLES `action` WRITE;
+/*!40000 ALTER TABLE `action` DISABLE KEYS */;
+/*!40000 ALTER TABLE `action` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alignment`
+--
+
+DROP TABLE IF EXISTS `alignment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alignment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `alignment` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alignment`
+--
+
+LOCK TABLES `alignment` WRITE;
+/*!40000 ALTER TABLE `alignment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alignment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `environment`
+--
+
+DROP TABLE IF EXISTS `environment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `environment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `environment` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `environment`
+--
+
+LOCK TABLES `environment` WRITE;
+/*!40000 ALTER TABLE `environment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `environment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `monster`
+--
+
+DROP TABLE IF EXISTS `monster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `monster` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT '',
+  `size` int(11) NOT NULL DEFAULT '0',
+  `environment` int(11) NOT NULL DEFAULT '0',
+  `alignment` int(11) NOT NULL DEFAULT '0',
+  `hit_points` int(11) NOT NULL DEFAULT '0',
+  `hit_dice` varchar(20) NOT NULL DEFAULT '0d0',
+  `speed` varchar(50) NOT NULL DEFAULT '0ft',
+  `condition_immunities` varchar(50) NOT NULL DEFAULT '',
+  `senses` varchar(50) NOT NULL DEFAULT '',
+  `languages` varchar(50) NOT NULL DEFAULT '',
+  `challenge_rating` int(11) NOT NULL DEFAULT '0',
+  `special_abilities` varchar(30) NOT NULL DEFAULT '' COMMENT 'comma-delimited list of ids for special abilities',
+  `legendary_actions` varchar(30) NOT NULL DEFAULT '' COMMENT 'comma-delimited list of ids for legendary_actions',
+  `actions` varchar(50) NOT NULL DEFAULT '' COMMENT 'comma-delimited list of ids for actions',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `monster`
+--
+
+LOCK TABLES `monster` WRITE;
+/*!40000 ALTER TABLE `monster` DISABLE KEYS */;
+/*!40000 ALTER TABLE `monster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `saves`
+--
+
+DROP TABLE IF EXISTS `saves`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `saves` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'the id for the monster',
+  `stat` int(11) NOT NULL DEFAULT '0' COMMENT 'stat as in stats table',
+  `points` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `saves`
+--
+
+LOCK TABLES `saves` WRITE;
+/*!40000 ALTER TABLE `saves` DISABLE KEYS */;
+/*!40000 ALTER TABLE `saves` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `size`
+--
+
+DROP TABLE IF EXISTS `size`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `size` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `size` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `size`
+--
+
+LOCK TABLES `size` WRITE;
+/*!40000 ALTER TABLE `size` DISABLE KEYS */;
+/*!40000 ALTER TABLE `size` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `skill`
+--
+
+DROP TABLE IF EXISTS `skill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `skill` (
+  `id` int(11) NOT NULL,
+  `skill` varchar(50) NOT NULL DEFAULT '',
+  `points` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `skill`
+--
+
+LOCK TABLES `skill` WRITE;
+/*!40000 ALTER TABLE `skill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `skill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stats`
+--
+
+DROP TABLE IF EXISTS `stats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stats` (
+  `id` int(11) NOT NULL,
+  `strength` int(11) NOT NULL DEFAULT '0',
+  `dexterity` int(11) NOT NULL DEFAULT '0',
+  `charisma` int(11) NOT NULL DEFAULT '0',
+  `constitution` int(11) NOT NULL DEFAULT '0',
+  `wisdom` int(11) NOT NULL DEFAULT '0',
+  `intelligence` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stats`
+--
+
+LOCK TABLES `stats` WRITE;
+/*!40000 ALTER TABLE `stats` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stats` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-04 14:51:15
--- parse.go
+++ parse.go
@@ -67,7 +67,3 @@ func parseMonsters(jsonString string) []Monster {
 	}
 	return monsters
 }
-
-func (m *Monster) unmarshalJson(data []byte) error {
-	return nil;
-}
