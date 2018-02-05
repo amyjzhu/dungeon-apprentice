ae5f70cb7b86545a85e61917bfadffee6e6a2720
amyjzhu <ajyzhu@gmail.com>
2018-02-04 17:25:22 -0800

    Hm

--- database.go
+++ database.go
@@ -7,6 +7,37 @@ import (
 	"log"
 )
 
+func insertAllEnums() {
+	db, err := sql.Open("mysql", "amy:zhu@tcp(127.0.0.1:3306/monster")
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer db.Close()
+
+	insertStat(db)
+	//insertSkill(db)
+	//insertEnvironment(db)
+//	insert(db)
+
+}
+
+func insertStat(db *sql.DB) {
+	statsNames := [6]string{"charisma", "strength", "constitution", "intelligence", "wisdom", "dexterity"}
+	stats := make(map[string]int)
+
+	stmt, err := db.Prepare("insert into stats `stat` values ?")
+	defer stmt.Close();
+	for i := 0; i < 6; i++ {
+		stmt.Exec(statsNames[i]);
+		if (err != nil) {
+			log.Fatal(err)
+		}
+		stats[statsNames[i]] = i
+	}
+
+}
+
+/*
 func connect() {
 	db, err := sql.Open("mysql", "amy:zhu@tcp(127.0.0.1:3306/monster")
 	if err != nil {
@@ -44,3 +75,4 @@ func connect() {
 func getIdForValue(db string, value string) {
 	stmt, err := db.Prepare("select id from ? where name=") // change db for consistency
 }
+*/
